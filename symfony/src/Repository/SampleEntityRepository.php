24f4c68136f398128b7af48ecb5c761f3af4e431
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-11-12 13:15:26 +0100

    feat: move docker .env to .env.dist, add some comments about its usage

--- symfony/src/Repository/SampleEntityRepository.php
+++ symfony/src/Repository/SampleEntityRepository.php
@@ -3,41 +3,15 @@
 namespace App\Repository;
 
 use App\Entity\SampleEntity;
-use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<SampleEntity>
+ * @author Wilhelm Zwertvaegher
  */
-class SampleEntityRepository extends ServiceEntityRepository
+interface SampleEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, SampleEntity::class);
-    }
+    public function findById(int $id): ?SampleEntity;
 
-    //    /**
-    //     * @return SampleEntity[] Returns an array of SampleEntity objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('s.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
+    public function findByName(string $name): array;
 
-    //    public function findOneBySomeField($value): ?SampleEntity
-    //    {
-    //        return $this->createQueryBuilder('s')
-    //            ->andWhere('s.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
+    public function save(SampleEntity $sampleEntity): void;
 }
