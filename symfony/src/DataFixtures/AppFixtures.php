f3a8e15f72a5845ca9296919fb0991cf48db718a
Wilhelm Zwertvaegher <wilhelm@l4m.fr>
2025-11-14 09:45:30 +0100

    feat(ci): add coverage check fail with 80% threshold

--- symfony/src/DataFixtures/AppFixtures.php
+++ symfony/src/DataFixtures/AppFixtures.php
@@ -6,6 +6,9 @@ use App\Entity\SampleEntity;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * @codeCoverageIgnore
+ */
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
